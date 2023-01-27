bd77c043e167c6e8bf398d9c224ceaeffaaf53e5
DENIVON95 <ivonin.denis@mail.ru>
2023-01-27 16:44:47 +0400

    fixes

--- src/test/java/MoviePosterManagerTest.java
+++ src/test/java/MoviePosterManagerTest.java
@@ -1,7 +1,5 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -40,29 +38,39 @@ public class MoviePosterManagerTest {
 
     @Test
     public void shouldReturnLastMoviesInRightOrder() {
-        Movie[] moviesToAdd = {new Movie("Красавица и чудовище"), new Movie("Аватар"),
-                new Movie("Титаник")};
+        Movie firstMovie = new Movie("Красавица и чудовище");
+        Movie secondMovie = new Movie("Аватар");
+        Movie thirdMovie = new Movie("Титаник");
+        Movie[] moviesToAdd = {firstMovie, secondMovie, thirdMovie};
+        Movie[] expectedMoviesToReturn = {thirdMovie, secondMovie, firstMovie};
 
         MoviePosterManager moviePosterManager = new MoviePosterManager();
-        moviePosterManager.addMovie(moviesToAdd[0]);
-        moviePosterManager.addMovie(moviesToAdd[1]);
-        moviePosterManager.addMovie(moviesToAdd[2]);
-        Movie[] foundedMovies = moviePosterManager.findLast();
-
-        for (int i = 0; i < foundedMovies.length; i++) {
-            assertEquals(moviesToAdd[foundedMovies.length - (i + 1)], foundedMovies[i]);
+        for (Movie movie : moviesToAdd) {
+            moviePosterManager.addMovie(movie);
         }
+        Movie[] foundMovies = moviePosterManager.findLast();
+        assertArrayEquals(expectedMoviesToReturn, foundMovies);
+
     }
 
     @Test
     public void shouldReturnOnlyTenLastMovies() {
-        Movie[] moviesToAdd = {new Movie("Красавица и чудовище"), new Movie("Аватар"),
-                new Movie("Титаник"), new Movie("Хатико"), new Movie("Зеленая миля"),
-                new Movie("Портрет Дориана Грея"), new Movie("Золушка"), new Movie("Хроники Нарнии"),
-                new Movie("Хроники Нарнии"), new Movie("Терминатор"), new Movie("Судья Дред"),
-                new Movie("Паразиты")
-        };
-        Movie[] expectedMoviesToReturn = Arrays.copyOfRange(moviesToAdd, 2, moviesToAdd.length);
+        Movie firstMovie = new Movie("Красавица и чудовище");
+        Movie secondMovie = new Movie("Аватар");
+        Movie thirdMovie = new Movie("Титаник");
+        Movie fourthMovie = new Movie("Хатико");
+        Movie fifthMovie = new Movie("Зеленая миля");
+        Movie sixthMovie = new Movie("Портрет Дориана Грея");
+        Movie seventhMovie = new Movie("Золушка");
+        Movie eighthMovie = new Movie("Хроники Нарнии");
+        Movie ninthMovie = new Movie("Хроники Нарнии 2");
+        Movie tenthMovie = new Movie("Терминатор");
+        Movie eleventhMovie = new Movie("Судья Дред");
+        Movie twelfthMovie = new Movie("Паразиты");
+        Movie[] moviesToAdd = {firstMovie, secondMovie, thirdMovie, fourthMovie, fifthMovie, sixthMovie, seventhMovie,
+                eighthMovie, ninthMovie, tenthMovie, eleventhMovie, twelfthMovie};
+        Movie[] expectedMoviesToReturn = {twelfthMovie, eleventhMovie, tenthMovie, ninthMovie, eighthMovie,
+                seventhMovie, sixthMovie, fifthMovie, fourthMovie, thirdMovie};
 
         MoviePosterManager moviePosterManager = new MoviePosterManager(10);
         for (Movie movie : moviesToAdd) {
@@ -71,8 +79,6 @@ public class MoviePosterManagerTest {
 
         Movie[] foundedMovies = moviePosterManager.findLast();
         assertEquals(10, foundedMovies.length);
-        for (int i = 0; i < foundedMovies.length; i++) {
-            assertEquals(expectedMoviesToReturn[foundedMovies.length - (i + 1)], foundedMovies[i]);
-        }
+        assertArrayEquals(expectedMoviesToReturn, foundedMovies);
     }
 }
